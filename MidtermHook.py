74ded541277dae5d65a545d9ceee8111044ecbb6
STIRELLA2 <90012377+STIRELLA2@users.noreply.github.com>
2021-11-09 09:12:48 -0500

    Update MidtermHook.py

--- MidtermHook.py
+++ MidtermHook.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__) # "__main__"
 
@@ -90,4 +91,4 @@ def page_not_found(error):
     return 'This page does not exist', 404
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=45456)
+  app.run(host='0.0.0.0', port=78789)
