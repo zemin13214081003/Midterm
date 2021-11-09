4e44b72fa3fc6f4a5192482c53fe6eaa8360f7da
STIRELLA2 <90012377+STIRELLA2@users.noreply.github.com>
2021-11-09 08:13:42 -0500

    Update MidtermHook.py

--- MidtermHook.py
+++ MidtermHook.py
@@ -90,4 +90,4 @@ def page_not_found(error):
     return 'This page does not exist', 404
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=753741)
+  app.run(host='0.0.0.0', port=1212123)
