c3fdebfb01950d414222d0b34707c2177967f7d0
STIRELLA2 <90012377+STIRELLA2@users.noreply.github.com>
2021-11-08 21:59:57 -0500

    Update MidtermHook.py

--- MidtermHook.py
+++ MidtermHook.py
@@ -39,7 +39,7 @@ def post(string):
 @app.route('/nlp1', methods=['POST'])
 def nlp1():
     data = request.form.get('string')
-    return render_templplate(data)
+    return render_template(data)
   
 #NLPService 2: remove numbers:
 @app.route('/2', methods=['POST'])
